e6194d696c206d15c7782ef0cca94cce664ddf6f
nishantg95 <nishant.groveris@gmail.com>
2016-10-02 10:25:24 -0500

    Now tracking velocity

--- mobile/src/main/java/teamrocket/androidautotamu/Main.java
+++ mobile/src/main/java/teamrocket/androidautotamu/Main.java
@@ -7,14 +7,27 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import java.lang.Object;
+import java.util.Date;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import static teamrocket.androidautotamu.R.id.time;
+
 public class Main extends Activity implements SensorEventListener {
     private float mLastX, mLastY, mLastZ;
     private boolean mInitialized; private SensorManager mSensorManager; private Sensor mAccelerometer; private final float NOISE = (float) 2.0;
+    private float avgx =0, avgy=0, avgz=0;
+    private Date startTime;
+    private long startTimeLong;
     /** Called when the activity is first created. */
 
     @Override
@@ -25,11 +38,13 @@ public class Main extends Activity implements SensorEventListener {
         mInitialized = false;
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_STATUS_ACCURACY_HIGH);
+        startTime = new Date();
+        startTimeLong =startTime.getTime();
     }
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_STATUS_ACCURACY_HIGH);
     }
     protected void onPause() {
         super.onPause();
@@ -44,10 +59,15 @@ public class Main extends Activity implements SensorEventListener {
         TextView tvX= (TextView)findViewById(R.id.x_axis);
         TextView tvY= (TextView)findViewById(R.id.y_axis);
         TextView tvZ= (TextView)findViewById(R.id.z_axis);
+        TextView tvAccel = (TextView) findViewById(R.id.acceleration);
+        TextView tvVel = (TextView) findViewById(R.id.velocity);
+
         ImageView iv = (ImageView)findViewById(R.id.image);
         float x = event.values[0];
         float y = event.values[1];
         float z = event.values[2];
+        double acceleration = 0;
+        double velocity = 0;
         if (!mInitialized) {
             mLastX = x;
             mLastY = y;
@@ -56,6 +76,11 @@ public class Main extends Activity implements SensorEventListener {
             tvY.setText("0.0");
             tvZ.setText("0.0");
             mInitialized = true;
+//            try {
+//                wait(2000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
         } else {
             float deltaX = Math.abs(mLastX - x);
             float deltaY = Math.abs(mLastY - y);
@@ -63,13 +88,27 @@ public class Main extends Activity implements SensorEventListener {
             if (deltaX < NOISE) deltaX = (float)0.0;
             if (deltaY < NOISE) deltaY = (float)0.0;
             if (deltaZ < NOISE) deltaZ = (float)0.0;
+            Date currentTime = new Date();
+            long current = currentTime.getTime();
+            long deltaT = (current - startTimeLong)/1000;
+            avgx += deltaX;
+            avgy += deltaY;
+            avgz += deltaZ;
+            double avgacceleration = (double)Math.sqrt(Math.pow(avgx, 2) + Math.pow(avgy, 2) + Math.pow(avgz, 2));
             mLastX = x;
             mLastY = y;
             mLastZ = z;
             tvX.setText(Float.toString(deltaX));
             tvY.setText(Float.toString(deltaY));
             tvZ.setText(Float.toString(deltaZ));
-            iv.setVisibility(View.VISIBLE);
+            acceleration = (double)Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2) + Math.pow(deltaZ, 2));
+            tvAccel.setText(acceleration+"m/s^2");
+            velocity = avgacceleration /deltaT;
+            tvVel.setText(velocity+" m/s"+"\n"+velocity * 2.2374+" miles/hour");
+
+            Log.d("",deltaX + "\t" + deltaY + "\t" + deltaZ + "\t" + acceleration);
+//            hj6
+            //iv.setVisibility(View.VISIBLE);
 //            if (deltaX > deltaY) {
 //                iv.setImageResource(R.drawable.horizontal);
 //            } else if (deltaY > deltaX) {
